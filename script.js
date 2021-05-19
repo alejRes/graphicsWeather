d9d42daedef94bc967f81b9d7b01e61ce4e230af
ignaciosein@gmail.com <ignaciosein@gmail.com>
2021-05-19 09:27:01 +0200

    he añadido los nuevos archivos

--- script.js
+++ script.js
@@ -1,104 +1,201 @@
-let tempActu = []
-let tempMax = []
-let tempMin = []
-let tempFeel = []
-let ejeY = []
-let txtCity;
-var myChart;
+let speed = [];
+let horas = [];
+let grafica2 = "";
+let nameCity = "";
+let myChart = "";
+let myChart2 = "";
+let myChart3 = "";
+let presion = [];
+let presionNMar = [];
+let presionNTierra = [];
+let tempMax = [];
+let tempMin = [];
+let tempNormal = [];
+let senseTermica = [];
 
-document.getElementById('btnSend').addEventListener('click', () => {
-    txtCity = document.getElementById('city').value
-    getResource(txtCity).then(data => data.forEach(element => {Char(element)}))
-        .then(() => {
-            paintChar()
-            document.getElementById('city').value=""
-        })
-})
+console.log(nameCity);
+ 
+document.getElementById("enviar").addEventListener("click", function () {
+  
+  let nameCity = document.getElementById("meterCiudad").value;
+  
+  document.getElementById("resultado").innerHTML = "";
 
-const getResource = async (city) => {
+ 
 
-    tempActu=[]
-    tempFeel=[]
-    tempMax=[]
-    tempMin=[]
-    ejeY = []
-    let res = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&cnt=15&appid=4d548be3703aebfa1659953c82c4642d`)
-    let data = await res.json();
-    console.log(data)
-    return data.list
+  tiempo(nameCity);
+ 
+  myChart  = new Chart(document.getElementById("myChart"),  config);
+  myChart2 = new Chart(document.getElementById("myChart2"), config2);
+  myChart3 = new Chart(document.getElementById("myChart3"), config3);
+  
+ 
+});
+
+const tiempo = async (ciudad) => {
+
+  let res = await fetch(
+    `http://api.openweathermap.org/data/2.5/forecast?q=${ciudad}&units=metric&appid=4d548be3703aebfa1659953c82c4642d`
+  );
+  let data = await res.json();
+ 
+  sacardatos(data.list);
+  document.getElementById("meterCiudad").value = ""
+};
+
+function sacardatos(datos) {
+  for (let index = 0; index < datos.length; index++) {
+
+    tempMax.push(datos[index].main.temp_max);
+    tempMin.push(datos[index].main.temp_min);
+    tempNormal.push(datos[index].main.temp);
+    speed.push(datos[index].wind.speed);
+
+    horas.push(datos[index].dt_txt);
+    presionNMar.push(datos[index].main.sea_level);
+    presionNTierra.push(datos[index].main.grnd_level);
+  }
 
 }
 
-const Char = ({ dt_txt, main }) => {
-    tempActu.push(main.temp)
-    tempMax.push(main.temp_max)
-    tempMin.push(main.temp_min)
-    tempFeel.push(main.feels_like)
-    ejeY.push(dt_txt)
-}
+ 
+ 
+const labels = horas;
+const data = {
+  labels: horas,
+  datasets: [
+    {
+      label: "Viento km/h",
+      data: speed,
+      borderColor: "blue",
+      backgroundColor: "blue",
+    },
+  ],
+};
 
-const paintChar = () => {
-    if(myChart){
-        myChart.destroy();
-    }
+const labels2 = presionNMar;
+const data2 = {
+  labels: presionNMar,
+  datasets: [
+    {
+      label: "Presion a nivel del suelo",
+      data: presionNTierra,
+      borderColor: "blue",
+      backgroundColor: "blue",
+    },
+    {
+      label: "Presion a nivel del mar",
+      data: presionNMar,
+      borderColor: "red",
+      backgroundColor: "red",
+    },
+  ],
+};
 
-    const labels = ejeY
-    const data = {
-        labels: labels,
-        datasets: [
-            {
-                label: 'temp Actual',
-                data: tempActu,
-                borderColor: 'red',
-                backgroundColor: ('red'),
-            },
-            {
-                label: 'temperatura máxima',
-                data: tempMax,
-                borderColor: 'blue',
-                backgroundColor: 'blue'
-            },
-            {
-                label: 'temperatura minima',
-                data: tempMin,
-                borderColor: 'aqua',
-                backgroundColor: 'aqua'
-            },
-            {
-                label: 'Sensacion térmica',
-                data: tempFeel,
-                borderColor: 'pink',
-                backgroundColor: 'pink'
-            }
-        ]
-    };
-    const config = {
-        type: 'bar',
-        data: data,
-        options: {
-            indexAxis: 'y',
-            
-            elements: {
-                bar: {
-                    borderColor: 'black',
-                    borderWidth: 2,
-                }
-            },
-            responsive: true,
-            plugins: {
-                legend: {
-                    position: 'right',
-                },
-                title: {
-                    display: true,
-                    text: `Temperaturas de ${txtCity}`
-                }
-            }
-        },
-    };
-    myChart = new Chart(
-        document.getElementById('myChart'),
-        config
-    );
-    return myChart
-}
+const labels3 = presionNMar;
+const data3 = {
+  labels: horas,
+  datasets: [
+    {
+      label: "Temperatura Maxima",
+      data: tempMax,
+      borderColor: "red",
+      backgroundColor: "red",
+    },
+    {
+      label: "Temperatura Minima",
+      data: tempMin,
+      borderColor: "green",
+      backgroundColor: "green",
+    },
+    {
+      label: "Temperatura Normal",
+      data: tempNormal,
+      borderColor: "yellow",
+      backgroundColor: "yellow",
+    },
+    {
+      label: "Temperatura Normal",
+      data: horas,
+      borderColor: "blue",
+      backgroundColor: "blue",
+    },
+  ],
+};
+
+const config3 = {
+  type: "bar",
+  data: data3,
+  options: {
+    ///
+    indexAxis: "y",
+    // Elements options apply to all of the options unless overridden in a dataset
+    // In this case, we are setting the border of each horizontal bar to be 2px wide
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "right",
+      },
+      title: {
+        display: true,
+        text: `${nameCity}`,
+      },
+    },
+  },
+};
+
+const config2 = {
+  type: "bar",
+  data: data2,
+  options: {
+    ///
+    indexAxis: "y",
+    // Elements options apply to all of the options unless overridden in a dataset
+    // In this case, we are setting the border of each horizontal bar to be 2px wide
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "right",
+      },
+      title: {
+        display: true,
+        text: `${nameCity}`,
+      },
+    },
+  },
+};
+
+const config = {
+  type: "bar",
+  data: data,
+  options: {
+    indexAxis: "y",
+    // Elements options apply to all of the options unless overridden in a dataset
+    // In this case, we are setting the border of each horizontal bar to be 2px wide
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "right",
+      },
+      title: {
+        display: true,
+        text: `${nameCity}`,
+      },
+    },
+  },
+};
